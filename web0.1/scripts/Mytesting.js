878d42a969ad5d623ba7ceba750e5abdb42107db
LeeBlu <pseshibe@gmail.com>
2016-10-10 09:41:23 +0200

    worked on the product page

--- web0.1/scripts/Mytesting.js
+++ web0.1/scripts/Mytesting.js
@@ -1,11 +1,19 @@
 ﻿$(document).ready(function () {
-    ("p").hide();
-    $("inner").click(function () {
+
+    $("p").hide();
+    $("#inner").click(function () {
         $(this).next().slideToggle(1000, function () {
             $("p").next().slideToggle(1000)
         });
-    });
+        
+        
+            //$("#p2").hide();
+            //$("#inner").click(function () {
+            //    $(this).next().slideToggle(300);
+            //});
 
-    
+       
+
+    });
 
 });
