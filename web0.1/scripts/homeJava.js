5516a251ebfbb9670763a4cb76bd06397ecf311b
ErroC <errolchab@gmail.com>
2016-10-10 15:44:59 +0200

    home page changes of code but function not changed

---
+++ web0.1/scripts/homeJava.js
@@ -0,0 +1,40 @@
+﻿
+
+      $(document).ready(function(){
+          $("#tray1").click(function(){
+              $("#panel").slideToggle("slow");
+          });
+      });
+
+
+
+
+      $(document).ready(function () {
+          $("#tray2").click(function () {
+              $("#panel2").slideToggle("slow");
+          });
+      });
+
+
+
+      $(document).ready(function () {
+          $("#tray3").click(function () {
+              $("#panel3").slideToggle("slow");
+          });
+      });
+
+
+
+      $(document).ready(function () {
+          $("#tray4").click(function () {
+              $("#panel4").slideToggle("slow");
+          });
+      });
+
+
+      $(document).ready(function () {
+          $("#tray5").click(function () {
+              $("#panel5").slideToggle("slow");
+          });
+      });
+
